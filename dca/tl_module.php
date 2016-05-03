c530dc107c63769a71a8ab2c1da25fc1d42d1494
Sebastian Buck <Sebastian Buck>
2016-05-03 19:28:53 +0200

    Added field to module

---
+++ dca/tl_module.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @package   CalendarHighlight
+ * @author    Sebastian Buck
+ * @license   LGPL
+ * @copyright Erdmann & Freunde
+ */
+
+
+ // Anpassung der Palette
+ $GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist'] = str_replace(
+     'perPage;',
+     'perPage,events_featured;',
+     $GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist']
+ );
+
+// Feld hinzufügen
+$GLOBALS['TL_DCA']['tl_module']['fields']['events_featured'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['events_featured'],
+	'default'                 => 'all_items',
+	'exclude'                 => true,
+	'inputType'               => 'select',
+	'options'                 => array('all_events', 'featured_events', 'unfeatured_events'),
+	'reference'               => &$GLOBALS['TL_LANG']['tl_module'],
+	'eval'                    => array('tl_class'=>'w50'),
+	'sql'                     => "varchar(18) NOT NULL default ''"
+);
---
+++ languages/de/tl_module.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @package   CalendarHighlight
+ * @author    Sebastian Buck
+ * @license   LGPL
+ * @copyright Erdmann & Freunde
+ */
+
+// Felder
+ $GLOBALS['TL_LANG']['tl_module']['events_featured'] = array('Hervorgehobene Termine','Hier legen Sie fest, wie hervorgehobene Termine gehandhabt werden.');
+
+// Options
+$GLOBALS['TL_LANG']['tl_module']['all_events'] = 'Alle Termine anzeigen';
+$GLOBALS['TL_LANG']['tl_module']['featured_events'] = 'Nur hervorgehobene Termine anzeigen';
+$GLOBALS['TL_LANG']['tl_module']['unfeatured_events'] = 'Hervorgehobene Termine überspringen';
