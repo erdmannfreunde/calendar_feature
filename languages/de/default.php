2e3af21c9d682621a49d3c5286d685075979fbbc
Sebastian Buck <Sebastian Buck>
2016-05-03 18:02:12 +0200

    Initial commit

---
+++ languages/de/default.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @package   CalendarHighlight
+ * @author    Sebastian Buck
+ * @license   LGPL
+ * @copyright Erdmann & Freunde
+ */
+
+
+/**
+ * Miscellaneous
+ */
+$GLOBALS['TL_LANG']['MSC'][''] = '';
