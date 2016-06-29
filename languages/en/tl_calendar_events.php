c2bbc9f0a280c17df2fad219adeea9cc1822b784
Dennis Erdmann <dennis@erdmann-freunde.de>
2016-06-29 11:53:23 +0200

    add english translation

---
+++ languages/en/tl_calendar_events.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @package   CalendarHighlight
+ * @author    Sebastian Buck
+ * @license   LGPL
+ * @copyright Erdmann & Freunde
+ */
+
+
+ $GLOBALS['TL_LANG']['tl_calendar_events']['featured'] = array('Feature item','Show the event item in a featured event list.');
